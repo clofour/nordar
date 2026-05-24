a93296d40a80f42d04936fbe15105fe41cc863ef
clofour <208706398+clofour@users.noreply.github.com>
2026-05-24 16:22:05 +0200

    Bug fixes

--- infra/ansible/prepare.sh
+++ infra/ansible/prepare.sh
@@ -1,7 +1,7 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 set -euo pipefail
 
-echo "$ANSIBLE_ANSIBLE_SSH_PRIVATE_KEY" > "$1/ansible"
+echo "$ANSIBLE_SSH_PRIVATE_KEY" > "$1/ansible"
 echo "$PKI_ROOT_CERTIFICATE" > "$1/root.crt"
 echo "$PKI_ANSIBLE_CERTIFICATE" > "$1/ansible.crt"
 echo "$PKI_ANSIBLE_PRIVATE_KEY" > "$1/ansible.key"
