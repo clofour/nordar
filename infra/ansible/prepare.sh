2b33b365bcf8ac129ba104f20d62497c4f81f0ab
clofour <208706398+clofour@users.noreply.github.com>
2026-05-24 22:02:50 +0200

    [skip actions] Bugfix

--- infra/ansible/prepare.sh
+++ infra/ansible/prepare.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-chmod 0700 "$1"
-chmod 0600 "$1/ansible" "$1/root.crt" "$1/ansible.crt" "$1/ansible.key" "$1/chain.crt"
-
+mkdir -p "$1"
 
 echo "$ANSIBLE_SSH_PRIVATE_KEY" > "$1/ansible"
 
@@ -11,3 +9,7 @@ echo "$PKI_ROOT_CERTIFICATE" > "$1/root.crt"
 echo "$PKI_ANSIBLE_CERTIFICATE" > "$1/ansible.crt"
 echo "$PKI_ANSIBLE_PRIVATE_KEY" > "$1/ansible.key"
 echo "$PKI_CHAIN_CERTIFICATE" > "$1/chain.crt"
+
+
+chmod 0700 "$1"
+chmod 0600 "$1/ansible" "$1/root.crt" "$1/ansible.crt" "$1/ansible.key" "$1/chain.crt"
