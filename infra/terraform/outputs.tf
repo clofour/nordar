096695307b496ccf47fbec3474dba441261c86e3
clofour <208706398+clofour@users.noreply.github.com>
2026-05-26 18:06:09 +0200

    [skip actions] Add Terraform output for node IPs

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,16 @@
 output "frontend_spaces_bucket" {
     value = digitalocean_spaces_bucket.frontend.name
 }
+
+
+output "database_proxy_nodes" {
+    value = { for k, v in digitalocean_droplet.database_proxy : k => v.ipv4_address }
+}
+
+output "database_nodes" {
+    value = { for k, v in digitalocean_droplet.database_nodes : k => v.ipv4_address }
+}
+
+output "backend_nodes" {
+    value = { for k, v in digitalocean_droplet.backend : k => v.ipv4_address }
+}
