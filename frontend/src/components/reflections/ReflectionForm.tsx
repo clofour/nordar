781b55d3b7897a5edbf329e562e90f0b1f2d8528
clofour <208706398+clofour@users.noreply.github.com>
2026-05-31 09:28:40 +0200

    Use list fields for ReflectionForm wins

--- frontend/src/components/reflections/ReflectionForm.tsx
+++ frontend/src/components/reflections/ReflectionForm.tsx
@@ -1,8 +1,10 @@
-import { Button, Group, Stack, Textarea, TextInput } from "@mantine/core";
-import { useForm, schemaResolver } from "@mantine/form";
+import type { ReactNode } from "react";
+import { Box, Button, Group, Input, Stack, Text, Textarea, TextInput, UnstyledButton } from "@mantine/core";
+import { useForm, schemaResolver, type UseFormReturnType } from "@mantine/form";
 import type { ReflectionCreate } from "@/api/models";
 import { PostApiReflectionCreateBody } from "@/api/endpoints/reflection/reflection.zod";
 import { postApiReflectionCreate } from "@/api/endpoints/reflection/reflection";
+import { IconMinus, IconPlus } from "@tabler/icons-react";
 
 interface ReflectionFormProps {
     flow: "create" | "edit";
@@ -10,10 +12,58 @@ interface ReflectionFormProps {
     initialValues?: ReflectionCreate;
 }
 
+interface AddButtonProps {
+    form: UseFormReturnType<any>;
+    type: string;
+    text: string;
+}
+
+interface RemoveButtonProps {
+    form: UseFormReturnType<any>;
+    type: string;
+    index: number;
+    field: ReactNode;
+}
+
+function AddButton({ form, type, text }: AddButtonProps) {
+    return (
+        <UnstyledButton w="100%" onClick={() => form.insertListItem(type, "")}>
+            <Group gap="xs">
+                <Box w="12">
+                    <IconPlus size={12} />
+                </Box>
+                <Text size="xs" c="dimmed">
+                    {text}
+                </Text>
+            </Group>
+        </UnstyledButton>
+    )
+}
+
+function RemoveButton({ form, type, index, field }: RemoveButtonProps) {
+    return (
+        <Group w="100%" gap="xs">
+            <Box w="12">
+                <UnstyledButton onClick={() => form.removeListItem(type, index)}>
+                    <IconMinus size={12} />
+                </UnstyledButton>
+            </Box>
+            <Box style={{ flex: 1 }}>
+                {field}
+            </Box>
+        </Group>
+
+    )
+}
+
 export default function ReflectionForm({ close, initialValues }: ReflectionFormProps) {
     const form = useForm({
         mode: "uncontrolled",
-        initialValues: initialValues,
+        initialValues: initialValues ?? {
+            positive: [],
+            negative: [],
+            improvement: []
+        },
         validate: schemaResolver(PostApiReflectionCreateBody, { sync: true }),
     });
 
@@ -39,14 +89,37 @@ export default function ReflectionForm({ close, initialValues }: ReflectionFormP
                         key={form.key("name")}
                         {...form.getInputProps("name")}
                     />
-                    <Textarea
+
+                    <Input.Wrapper
                         label="Wins"
                         description="What went well?"
-                        placeholder="Be healthy"
                         required
-                        key={form.key("positive")}
-                        {...form.getInputProps("positive")}
-                    />
+                    >
+                        <Stack gap="xs" mt="4">
+                            {form.getValues().positive.map((item, index) => (
+                                <RemoveButton
+                                    form={form}
+                                    type="positive"
+                                    index={index}
+                                    field={
+                                        <TextInput
+                                            placeholder="Be healthy"
+                                            key={form.key(`positive.${index}`)}
+                                            {...form.getInputProps(`positive.${index}`)}
+                                            w="100%"
+                                        />
+                                    }
+                                />
+
+                            ))}
+                            <AddButton
+                                form={form}
+                                type="positive"
+                                text="Add positive"
+                            />
+                        </Stack>
+                    </Input.Wrapper>
+
                     <Textarea
                         label="Challenges"
                         description="What went wrong?"
