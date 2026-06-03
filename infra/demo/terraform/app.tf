0eb6cd25ef31ec177410d906e3961dcc3a53a25a
clofour <208706398+clofour@users.noreply.github.com>
2026-06-03 11:20:12 +0200

    Complete demo Terraform manifests

--- infra/demo/terraform/app.tf
+++ infra/demo/terraform/app.tf
@@ -9,34 +9,43 @@ resource "digitalocean_app" "main" {
                     name = "frontend"
                 }
                 match {
-
+                    authority {
+                      exact = "${var.frontend_subdomain}.${var.domain}"
+                    }
+                    path {
+                      prefix = "/"
+                    }
                 }
             }
 
             rule {
                 component {
                     name = "backend"
+                    preserve_path_prefix = true
                 }
                 match {
-
+                    authority {
+                      exact = "${var.backend_subdomain}.${var.domain}"
+                    }
+                    path {
+                      prefix = "/"
+                    }
                 }
-            }
-        }
-
-        job {
-            name = "migrations"
-            kind = "POST_DEPLOY"
-
-            github {
-                branch = "main"
-                deploy_on_push = true
-                repo = "clofour/nordar"
             }
         }
 
         static_site {
             name = "frontend"
             build_command = "npm run build"
+
+            environment_slug = "node-js"
+            env {
+                key = "VITE_API_ORIGIN"
+                scope = "BUILD_TIME"
+                value = "${var.backend_subdomain}.${var.domain}"
+            }
+
+            catchall_document = "index.html"
 
             github {
                 branch = "main"
@@ -47,6 +56,19 @@ resource "digitalocean_app" "main" {
 
         service {
             name = "backend"
+
+            environment_slug = "dotnet"
+            env {
+                key = "ConnectionStrings__Default"
+                scope = "RUN_TIME"
+                value = "${database.DATABASE_URL}"
+            }
+            env {
+                key = "Origins__Frontend"
+                scope = "RUN_AND_BUILD_TIME"
+                value = "${var.frontend_subdomain}.${var.domain}"
+            }
+
             instance_count = 2
             instance_size_slug = "apps-s-1vcpu-1gb"
 
@@ -67,6 +89,25 @@ resource "digitalocean_app" "main" {
             engine = "PG"
             version = 18
             production = false
+        }
+
+        job {
+            name = "migrations"
+            kind = "POST_DEPLOY"
+
+            environment_slug = "dotnet"
+            build_command = ""
+            run_command = ""
+
+            instance_count = 1
+            instance_size_slug = "apps-s-1vcpu-1gb"
+
+            github {
+                branch = "main"
+                deploy_on_push = true
+                repo = "clofour/nordar"
+            }
+            source_dir = "app/backend"
         }
     }
 }
--- infra/demo/terraform/variables.tf
+++ infra/demo/terraform/variables.tf
@@ -16,23 +16,3 @@ variable "region" {
     type = string
     default = "fra1"
 }
-
-variable "backend_count" {
-    type = number
-    default = 2
-}
-
-variable "database_proxy_count" {
-    type = number
-    default = 2
-}
-
-variable "database_count" {
-    type = number
-    default = 3
-}
-
-variable "droplet_size" {
-    type = string
-    default = "s-1vcpu-2gb"
-}
