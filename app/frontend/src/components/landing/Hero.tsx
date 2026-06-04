0bd69b377e46807708aca104a068315fb7f03b5c
clofour <208706398+clofour@users.noreply.github.com>
2026-06-04 18:56:12 +0200

    Fix Hero.tsx transform bug

--- app/frontend/src/components/landing/Hero.tsx
+++ app/frontend/src/components/landing/Hero.tsx
@@ -3,9 +3,9 @@ import classes from "./Hero.module.css";
 export default function Hero() {
     return (
         <div className={classes.hero}>
-            <div className={classes.}>
+            {/* <div className={classes.}>
                 
-            </div>
+            </div> */}
         </div>
     );
 }
