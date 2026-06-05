6ee77d90f3145d49ae4b38ee233a574b62ad9b30
clofour <208706398+clofour@users.noreply.github.com>
2026-06-05 21:58:41 +0200

    Finalize core FAQ

--- app/frontend/src/components/landing/FAQ.tsx
+++ app/frontend/src/components/landing/FAQ.tsx
@@ -1,9 +1,35 @@
-import { Button, type ButtonVariant } from "@mantine/core";
+import { Accordion, Button, type ButtonVariant } from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons-react";
 import { Link } from "react-router";
 
 export default function FAQ() {
+    const faqs = [
+        {
+            question: "Hello1",
+            answer: "Hi!"
+        },
+        {
+            question: "Hello2",
+            answer: "Hi!"
+        },
+        {
+            question: "Hello3",
+            answer: "Hi!"
+        },
+        {
+            question: "Hello4",
+            answer: "Hi!"
+        },
+    ];
+
     return (
-        "Hi!"
+        <Accordion variant="separated" >
+            {faqs.map((faq) => (
+                <Accordion.Item key={faq.question} value={faq.question}>
+                    <Accordion.Control>{faq.question}</Accordion.Control>
+                    <Accordion.Panel>{faq.answer}</Accordion.Panel>
+                </Accordion.Item>
+            ))}
+        </Accordion>
     );
 }
