a6e9835e8d8e713fb7fdf22efe9fa92bdad159af
clofour <208706398+clofour@users.noreply.github.com>
2026-06-05 22:49:20 +0200

    Finalize core closing CTA

--- app/frontend/src/components/landing/ClosingCTA.tsx
+++ app/frontend/src/components/landing/ClosingCTA.tsx
@@ -1,9 +1,21 @@
-import { Button, type ButtonVariant } from "@mantine/core";
+import { Button, Stack, type ButtonVariant } from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons-react";
 import { Link } from "react-router";
+import Logo, { LogoLayout } from "../shared/Logo/Logo";
+import LinkButton from "./LinkButton";
 
 export default function ClosingCTA() {
     return (
-        "Hi!"
+        <Stack justify="center" align="center" ta="center">
+            <Logo layout={LogoLayout.Icon} />
+            <h2>Placeholder</h2>
+            <p>Placeholder</p>
+            <LinkButton
+                variant="filled"
+                label="Sign Up"
+                to="/auth/signup"
+            />
+            <p>Placeholder</p>
+        </Stack>
     );
 }
