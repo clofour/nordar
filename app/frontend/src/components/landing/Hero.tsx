ee187007219ca055b7c919052b4857c4410c76cd
clofour <208706398+clofour@users.noreply.github.com>
2026-06-05 20:46:51 +0200

    Get core Hero structure down

--- app/frontend/src/components/landing/Hero.tsx
+++ app/frontend/src/components/landing/Hero.tsx
@@ -1,11 +1,27 @@
+import { Group } from "@mantine/core";
 import classes from "./Hero.module.css";
+import LinkButton from "./LinkButton";
 
 export default function Hero() {
     return (
         <div className={classes.hero}>
-            {/* <div className={classes.}>
-                
-            </div> */}
+            <small>Placeholder</small>
+            <h1>Placeholder</h1>
+            <p>Placeholder</p>
+
+            <div className={classes.heroCTA}>
+                <LinkButton
+                    variant="default"
+                    label="Learn more"
+                    to="#tbd"
+                />
+                <LinkButton
+                    variant="filled"
+                    label="Sign Up"
+                    to="/auth/signup"
+                />
+            </div>
+
         </div>
     );
 }
