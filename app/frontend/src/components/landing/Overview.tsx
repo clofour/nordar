d0de4f7dc0e809c8f191e41be6343961966a5b90
clofour <208706398+clofour@users.noreply.github.com>
2026-06-08 23:11:17 +0200

    Progress on landing page

--- app/frontend/src/components/landing/Overview.tsx
+++ app/frontend/src/components/landing/Overview.tsx
@@ -7,37 +7,35 @@ import { GoalType } from "@/api/models";
 import GoalCard from "./GoalCard";
 
 export default function Overview() {
-	const goals = [
-		{
-			name: "Be healthy",
-			bearings: [
-				{
-					name: "Sleep early",
-					movements: [
-						{ name: "Wind-down alarm", time: "9:00 PM" },
-						{ name: "No screens", time: "after 10:30" },
-						{ name: "Lights out", time: "11:00 PM" }
-					]
-				},
-				{
-					name: "Do some exercise",
-					movements: [
-						{ name: "Walk 8.000 steps", time: "daily" },
-						{ name: "Strength training", time: "2x / wk" },
-						{ name: "Morning stretch", time: "8:00 AM" }
-					]
-				},
-				{
-					name: "Eat a healthy diet",
-					movements: [
-						{ name: "Cook dinner", time: "5 nights" },
-						{ name: "Prioritize protein", time: "each meal" },
-						{ name: "No late snacks", time: "after 08:00 PM" }
-					]
-				}
-			]
-		}
-	]
+	const goals = {
+		name: "Be healthy",
+		bearings: [
+			{
+				name: "Sleep early",
+				movements: [
+					{ name: "Wind-down alarm", time: "9:00 PM" },
+					{ name: "No screens", time: "after 10:30" },
+					{ name: "Lights out", time: "11:00 PM" }
+				]
+			},
+			{
+				name: "Do some exercise",
+				movements: [
+					{ name: "Walk 8.000 steps", time: "daily" },
+					{ name: "Strength training", time: "2x / wk" },
+					{ name: "Morning stretch", time: "8:00 AM" }
+				]
+			},
+			{
+				name: "Eat a healthy diet",
+				movements: [
+					{ name: "Cook dinner", time: "5 nights" },
+					{ name: "Prioritize protein", time: "each meal" },
+					{ name: "No late snacks", time: "after 08:00 PM" }
+				]
+			}
+		]
+	}
 
 	return (
 		<Module>
@@ -49,24 +47,22 @@ export default function Overview() {
 				<div>
 					<GoalExplanation type={GoalType.NorthStar} />
 					<Stack h="200px" align="center" justify="center">
-						{goals.map((northStar) =>
-							<GoalCard type={GoalType.NorthStar} text={northStar.name} />
-						)}
+						<GoalCard type={GoalType.NorthStar} text={goals.name} />
 					</Stack>
 				</div>
 				<div>
 					<GoalExplanation type={GoalType.Bearing} />
 					<Stack h="200px" align="center" justify="center">
-						{goals.map((northStar) =>
-							<GoalCard type={GoalType.NorthStar} text={northStar.name} />
+						{goals.bearings.map((bearing) =>
+							<GoalCard type={GoalType.Bearing} text={bearing.name} />
 						)}
 					</Stack>
 				</div>
 				<div>
 					<GoalExplanation type={GoalType.Movement} />
 					<Stack h="200px" align="center" justify="center">
-						{goals.map((northStar) =>
-							<GoalCard type={GoalType.NorthStar} text={northStar.name} />
+						{goals.bearings.map((northStar) =>
+							<GoalCard type={GoalType.Movement} text={northStar.name} />
 						)}
 					</Stack>
 				</div>
