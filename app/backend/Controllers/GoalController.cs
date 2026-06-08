2e51f5bc05545ac46b32e1bb37ef0433e71a6167
clofour <208706398+clofour@users.noreply.github.com>
2026-06-08 19:35:51 +0200

    Split up GoalController into GoalService

--- app/backend/Controllers/GoalController.cs
+++ app/backend/Controllers/GoalController.cs
@@ -21,7 +21,7 @@ namespace backend.Controllers
     {
         [HttpGet]
         [ProducesResponseType(typeof(List<NorthStarGet>), StatusCodes.Status200OK, "application/json")]
-        public async Task<ActionResult> Get()
+        public async Task<ActionResult> List()
         {
             var user = await userManager.GetUserAsync(User);
             if (user == null)
@@ -29,17 +29,8 @@ namespace backend.Controllers
                 return Forbid();
             }
 
-            await appDbContext.Entry(user)
-                .Collection(user => user.NorthStars)
-                .Query()
-                .Include(northStar => northStar.Bearings)
-                    .ThenInclude(bearing => bearing.Movements)
-                .LoadAsync();
-
-            List<NorthStar> northStars = user.NorthStars;
-            List<NorthStarGet> northStarsDTO = mapper.Map<List<NorthStarGet>>(northStars);
-
-            return Ok(northStarsDTO);
+            ServiceResult serviceResult = await goalService.List(user);
+            return ServiceBoundaryHelper.ConvertToActionResult(serviceResult);
         }
 
         [HttpGet]
@@ -52,14 +43,8 @@ namespace backend.Controllers
                 return Forbid();
             }
 
-            GoalStats goalStats = new GoalStats
-            {
-                NorthStarCount = await goalService.CountGoals<NorthStar>(user),
-                BearingCount = await goalService.CountGoals<Bearing>(user),
-                MovementCount = await goalService.CountGoals<Movement>(user)
-            };
-
-            return Ok(goalStats);
+            ServiceResult serviceResult = await goalService.Stats(user);
+            return ServiceBoundaryHelper.ConvertToActionResult(serviceResult);
         }
 
         [HttpPost]
@@ -71,14 +56,8 @@ namespace backend.Controllers
                 return Forbid();
             }
 
-            NorthStar northStar = new NorthStar();
-            mapper.Map(northStarCreate, northStar);
-            northStar.User = user;
-
-            appDbContext.NorthStars.Add(northStar);
-            await appDbContext.SaveChangesAsync();
-
-            return Ok();
+            ServiceResult serviceResult = await goalService.CreateNorthStar(user.Id, northStarCreate);
+            return ServiceBoundaryHelper.ConvertToActionResult(serviceResult);
         }
 
         [HttpPost]
@@ -90,23 +69,8 @@ namespace backend.Controllers
                 return Forbid();
             }
 
-            NorthStar? parent = await goalService.FindParent<NorthStar>(user, bearingCreate.NorthStarId);
-            if (parent == null)
-            {
-                return NotFound("The parent goal does not exist.");
-            }
-
-            await appDbContext.Entry(parent).Collection(northStar => northStar.Bearings).LoadAsync();
-
-            Bearing bearing = new Bearing();
-            mapper.Map(bearingCreate, bearing);
-            bearing.User = user;
-            bearing.NorthStar = parent;
-
-            appDbContext.Bearings.Add(bearing);
-            await appDbContext.SaveChangesAsync();
-
-            return Ok();
+            ServiceResult serviceResult = await goalService.CreateBearing(user, bearingCreate);
+            return ServiceBoundaryHelper.ConvertToActionResult(serviceResult);
         }
 
         [HttpPost]
@@ -118,23 +82,8 @@ namespace backend.Controllers
                 return Forbid();
             }
 
-            Bearing? parent = await goalService.FindParent<Bearing>(user, movementCreate.BearingId);
-            if (parent == null)
-            {
-                return NotFound("The parent goal does not exist.");
-            }
-
-            await appDbContext.Entry(parent).Collection(bearing => bearing.Movements).LoadAsync();
-
-            Movement movement = new Movement();
-            mapper.Map(movementCreate, movement);
-            movement.User = user;
-            movement.Bearing = parent;
-
-            appDbContext.Movements.Add(movement);
-            await appDbContext.SaveChangesAsync();
-
-            return Ok();
+            ServiceResult serviceResult = await goalService.CreateMovement(user, movementCreate);
+            return ServiceBoundaryHelper.ConvertToActionResult(serviceResult);
         }
 
         [HttpPost]
@@ -146,17 +95,8 @@ namespace backend.Controllers
                 return Forbid();
             }
 
-            int goalsDeleted = await goalService.ResolveGoalDbSet(goalType)
-                .Where(goal => goal.UserId == user.Id && goal.Id == id)
-                .ExecuteDeleteAsync();
-
-            if (goalsDeleted == 1)
-            {
-                return Ok();
-            }
-            else {
-                return NotFound();
-            }
+            ServiceResult serviceResult = await goalService.Delete(user.Id, id, goalType);
+            return ServiceBoundaryHelper.ConvertToActionResult(serviceResult);
         }
     }
 }
