91a1e8c555e8bce475f501f55704fcfd625ac943
clofour <208706398+clofour@users.noreply.github.com>
2026-06-08 20:11:51 +0200

    Ditto

--- app/frontend/src/pages/Reflections.tsx
+++ app/frontend/src/pages/Reflections.tsx
@@ -1,5 +1,4 @@
-import { useGetApiReflectionList } from "@/api/endpoints/reflection/reflection";
-import { getGetApiReflectionListResponseMock } from "@/api/endpoints/reflection/reflection.msw";
+import { useListReflections } from "@/api/endpoints/reflection/reflection";
 import Bar from "@/components/reflections/Bar";
 import ReflectionForm from "@/components/reflections/ReflectionForm";
 import Stats from "@/components/reflections/Stats";
@@ -11,7 +10,7 @@ import { IconPlus } from "@tabler/icons-react";
 export default function Reflections() {
 	const [opened, { open, close }] = useDisclosure(false);
 
-	const { data: response, error, isLoading, mutate } = useGetApiReflectionList();
+	const { data: response, error, isLoading, mutate } = useListReflections();
 
 	const aspects = [
 		{ label: "Wins", value: "positive" },
