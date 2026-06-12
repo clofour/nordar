07dd9c8513759fe1937a9b7df9dfb6e1aadbbe35
clofour <208706398+clofour@users.noreply.github.com>
2026-06-12 20:24:17 +0200

    Progress on landing page

--- app/frontend/src/components/landing/GoalExplanation.tsx
+++ app/frontend/src/components/landing/GoalExplanation.tsx
@@ -1,6 +1,9 @@
-import { Stack } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
 import classes from "./GoalExplanation.module.css";
 import { GoalType } from "@/api/models";
+import { text } from "@/data/text";
+import { theme } from "@/data/theme";
+import type { CSSProperties } from "react";
 
 type GoalExplanationProps = {
 	type: GoalType;
@@ -8,15 +11,17 @@ type GoalExplanationProps = {
 
 export function GoalExplanation({ type }: GoalExplanationProps) {
 	const descriptions = {
-		[GoalType.NorthStar]: "North north",
-		[GoalType.Bearing]: "Norh th",
-		[GoalType.Movement]: "eow",
+		[GoalType.NorthStar]: "Your highest ambitions. Set it once, and work on it for years.",
+		[GoalType.Bearing]: "A few directions to pull you towards your North Stars.",
+		[GoalType.Movement]: "Small actions to accomplish your Bearings.",
 	};
 
 	return (
 		<Stack gap="0" ta="left">
-			<div className={classes.type}>{type}</div>
-			<div>{descriptions[type]}</div>
+			<div className={classes.type} style={{
+				"--explanation-color": theme.colors.goal[type],
+			} as CSSProperties}>{text[type]}</div>
+			<Text c="dimmed">{descriptions[type]}</Text>
 		</Stack>
 	);
 }
