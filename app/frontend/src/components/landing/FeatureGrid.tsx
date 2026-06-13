7e5862bcfd7e0eb93b299150f0b4513cbc72af54
clofour <208706398+clofour@users.noreply.github.com>
2026-06-13 14:27:43 +0200

    Progress on Feature Grid

--- app/frontend/src/components/landing/FeatureGrid.tsx
+++ app/frontend/src/components/landing/FeatureGrid.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, type ButtonVariant } from "@mantine/core";
+import { Button, Grid, Group, Stack, type ButtonVariant } from "@mantine/core";
 import { IconCalendar, IconChartBar, IconNotebook, IconTarget } from "@tabler/icons-react";
 import { Link } from "react-router";
 import Card from "./Card";
@@ -8,26 +8,26 @@ export default function FeatureGrid() {
 		{
 			name: "Goals",
 			description: "I don't know",
-			icon: IconTarget,
+			Icon: IconTarget,
 			span: 9,
 		},
 		{
 			name: "Calendar",
 			description: "I don't know",
-			icon: IconCalendar,
+			Icon: IconCalendar,
 			span: 3,
 		},
 		{
 			name: "Reflections",
 			description: "I don't know",
-			icon: IconNotebook,
+			Icon: IconNotebook,
 			color: "teal",
 			span: 6,
 		},
 		{
 			name: "Analytics",
 			description: "I don't know",
-			icon: IconChartBar,
+			Icon: IconChartBar,
 			span: 6,
 		}
 	];
@@ -37,8 +37,14 @@ export default function FeatureGrid() {
 			{features.map((feature) =>
 				<Grid.Col span={{ base: 12, sm: feature.span }}>
 					<Card>
-						<div>{feature.name}</div>
-						<div>{feature.description}</div>
+						<Stack>
+							<Group>
+								<feature.Icon size={22} />
+								<div>{feature.name}</div>
+							</Group>
+
+							<div>{feature.description}</div>
+						</Stack>
 					</Card>
 				</Grid.Col>
 			)}
