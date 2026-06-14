01b2e83225c61faf68079bb11acc14f6a9c6c34d
clofour <208706398+clofour@users.noreply.github.com>
2026-06-14 10:51:34 +0200

    Add order prop to reveal

--- app/frontend/src/hooks/useReveal.ts
+++ app/frontend/src/hooks/useReveal.ts
@@ -3,12 +3,14 @@ import clsx from "clsx";
 import { useIntersection } from "@mantine/hooks";
 import { useRef } from "react";
 
+type Order = 1 | 2 | 3 | 4;
+
 interface UseRevealReturn {
     ref: (node: HTMLElement | null) => void;
     className: string;
 }
 
-export function useReveal(): UseRevealReturn {
+export function useReveal(order: Order = 1): UseRevealReturn {
     const revealed = useRef(false);
     const { ref, entry } = useIntersection({
         threshold: 0.1,
@@ -20,6 +22,6 @@ export function useReveal(): UseRevealReturn {
 
     return {
         ref,
-        className: clsx(classes.reveal, revealed.current ? classes.revealed : "")
+        className: clsx(classes.reveal, classes[`order${order}`], revealed.current ? classes.revealed : "")
     }
 }
