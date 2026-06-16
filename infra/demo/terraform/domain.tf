fdf05182dd67c3a60561904a162cf966d86c1b29
clofour <208706398+clofour@users.noreply.github.com>
2026-06-16 20:23:01 +0200

    Fix "data needs to end with a dot (.)" error in demo deployment

--- infra/demo/terraform/domain.tf
+++ infra/demo/terraform/domain.tf
@@ -6,12 +6,12 @@ resource "digitalocean_record" "frontend" {
     domain = data.digitalocean_domain.main.id
     type = "CNAME"
     name = var.frontend_subdomain
-    value = digitalocean_app.main.live_domain
+    value = "${digitalocean_app.main.live_domain}"
 }
 
 resource "digitalocean_record" "backend" {
     domain = data.digitalocean_domain.main.id
     type = "CNAME"
     name = var.backend_subdomain
-    value = digitalocean_app.main.live_domain
+    value = "${digitalocean_app.main.live_domain}."
 }
