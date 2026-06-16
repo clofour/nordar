e43984e5f2b3e9e5d69d7bfc3d28a2ca0766f601
clofour <208706398+clofour@users.noreply.github.com>
2026-06-16 20:08:20 +0200

    Fix domain ref in demo deployment

--- infra/demo/terraform/domain.tf
+++ infra/demo/terraform/domain.tf
@@ -3,14 +3,14 @@ resource "digitalocean_domain" "main" {
 }
 
 resource "digitalocean_record" "frontend" {
-    domain = digitalocean_domain.main.default.id
+    domain = digitalocean_domain.main.id
     type = "CNAME"
     name = var.frontend_subdomain
     value = digitalocean_app.main.live_domain
 }
 
 resource "digitalocean_record" "backend" {
-    domain = digitalocean_domain.main.default.id
+    domain = digitalocean_domain.main.id
     type = "CNAME"
     name = var.backend_subdomain
     value = digitalocean_app.main.live_domain
