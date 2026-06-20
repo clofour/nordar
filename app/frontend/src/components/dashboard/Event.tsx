0241167ecfc5f934d4e7586767e792cb3240b6c7
clofour <208706398+clofour@users.noreply.github.com>
2026-06-20 16:54:48 +0200

    Fix event mutation

--- app/frontend/src/components/dashboard/Event.tsx
+++ app/frontend/src/components/dashboard/Event.tsx
@@ -4,6 +4,8 @@ import type { ScheduleEventData } from "@mantine/schedule";
 import { useEffect, useState } from "react";
 import { EventTypes } from "@/metadata/events";
 import {
+	invalidateGetOnetimeInstanceState,
+	invalidateGetRecurringInstanceState,
 	useGetOnetimeInstanceState,
 	useGetRecurringInstanceState,
 	useSetOnetimeInstanceState,
@@ -11,12 +13,14 @@ import {
 } from "@/api/endpoints/event/event";
 import { NotificationType, useNotification } from "@/helpers";
 import { getErrorMessage } from "@/data/error";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface EventProps {
 	event: ScheduleEventData & EventGet;
 }
 
 export default function Event({ event }: EventProps) {
+	const queryClient = useQueryClient();
 	const notify = useNotification();
 
 	const [checked, setChecked] = useState(false);
@@ -37,8 +41,8 @@ export default function Event({ event }: EventProps) {
 		error,
 		isLoading,
 	} = event.type == "recurring" && "recurringEventId" in event && "recurrenceId" in event
-		? useGetRecurringInstanceState(event.recurringEventId, new Date(event.recurrenceId).toISOString())
-		: useGetOnetimeInstanceState(event.id);
+			? useGetRecurringInstanceState(event.recurringEventId, new Date(event.recurrenceId).toISOString())
+			: useGetOnetimeInstanceState(event.id);
 	useEffect(() => {
 		if (response?.data) {
 			setChecked(getBoolFromState(response.data.eventState));
@@ -57,10 +61,20 @@ export default function Event({ event }: EventProps) {
 		notify(NotificationType.Error, getErrorMessage(error));
 	};
 	const onetimeMutation = useSetOnetimeInstanceState({
-		mutation: { onError: onMutationError },
+		mutation: {
+			onError: onMutationError,
+			onSuccess: () => {
+				invalidateGetOnetimeInstanceState(queryClient, event.id);
+			}
+		},
 	});
 	const recurringMutation = useSetRecurringInstanceState({
-		mutation: { onError: onMutationError },
+		mutation: {
+			onError: onMutationError,
+			onSuccess: () => {
+				invalidateGetRecurringInstanceState(queryClient, event.id, new Date(event.recurrenceId ?? "").toISOString());
+			}
+		},
 	});
 	const onChange = async (checkBoxEvent: React.ChangeEvent<HTMLInputElement>) => {
 		const newCheckboxValue = checkBoxEvent.currentTarget.checked;
@@ -82,7 +96,7 @@ export default function Event({ event }: EventProps) {
 	return (
 		<Paper key={event.title} p="sm" withBorder>
 			<Group>
-				<Checkbox checked={checked} onChange={onChange} radius="xl" />
+				<Checkbox checked={checked} disabled={isLoading} onChange={onChange} radius="xl" />
 				<Text size="sm">{event.title}</Text>
 			</Group>
 		</Paper>
